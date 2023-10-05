83398ebac2f1b778552e66e2f4d906b813ed0855
Pranav21-pav <147014503+Pranav21-pav@users.noreply.github.com>
2023-10-04 22:48:12 -0500

    Publish To Github

--- getUserInput.js
+++ getUserInput.js
@@ -1,1 +1,7 @@
-
+const questions = [
+    "How many miles do you drive on average per day? A)0-5 B)5-10 C)10-20 D)20+",
+    "How long are your showers? A)0-5 B)5-15 C)15+",
+    "How often do you order takeout? A)0-2 B)2-4 C)4+",
+    "What type of car do you drive? A) No car B) Gas C) Electric D) Hybrid",
+    "How do you get your home energy? A) Solar Panels B) Power Grid C) Green Electric Plan"
+]
