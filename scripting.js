4564c3250dd0fb30a09187bc3a6b658f504003cb
Pranav21-pav <147014503+Pranav21-pav@users.noreply.github.com>
2023-10-05 10:40:24 -0500

    M

--- scripting.js
+++ scripting.js
@@ -4,4 +4,35 @@ const questions = [
     "How often do you order takeout? A)0-2 B)2-4 C)4+",
     "What type of car do you drive? A) No car B) Gas C) Electric D) Hybrid",
     "How do you get your home energy? A) Solar Panels B) Power Grid C) Green Electric Plan"
-]
+];
+
+let currentQuestionIndex = 0; // Index of the current question
+
+const questionContainer = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const userResponseElement = document.getElementById("user-response");
+const submitButton = document.getElementById("submit-button");
+
+// Function to display the current question
+function displayQuestion() {
+    if (currentQuestionIndex < questions.length) {
+        questionElement.textContent = questions[currentQuestionIndex];
+        userResponseElement.value = ""; // Clear the input field
+    } else {
+        questionContainer.style.display = "none"; // Hide the question container when all questions are answered
+        displayResults(); // Call a function to display results or provide feedback
+    }
+}
+
+// Event listener for the submit button
+submitButton.addEventListener("click", function () {
+    const userResponse = userResponseElement.value;
+    
+    // Process userResponse, e.g., save it or perform calculations
+    
+    currentQuestionIndex++; // Move to the next question
+    displayQuestion(); // Display the next question
+});
+
+// Initialize the first question
+displayQuestion();
