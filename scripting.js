762ca76ed897ce9e808666e9f9c206eea44b9730
Pranav21-pav <147014503+Pranav21-pav@users.noreply.github.com>
2023-10-05 19:58:06 -0500

    Reverted

--- scripting.js
+++ scripting.js
@@ -1,45 +1,41 @@
-const questions = [
-    "How many miles do you drive on average per day? A)0-5 B)5-10 C)10-20 D)20+",
-    "How long are your showers? A)0-5 B)5-15 C)15+",
-    "How often do you order takeout? A)0-2 B)2-4 C)4+",
-    "What type of car do you drive? A) No car B) Gas C) Electric D) Hybrid",
-    "How do you get your home energy? A) Solar Panels B) Power Grid C) Green Electric Plan"
-];
+    const questions = [
+        "How many miles do you drive on average per day? A)0-5 B)5-10 C)10-20 D)20+",
+        "How long are your showers? A)0-5 B)5-15 C)15+",
+        "How often do you order takeout? A)0-2 B)2-4 C)4+",
+        "What type of car do you drive? A) No car B) Gas C) Electric D) Hybrid",
+        "How do you get your home energy? A) Solar Panels B) Power Grid C) Green Electric Plan"
+    ];
 
-let currentQuestionIndex = 0; // Index of the current question
+    let currentQuestionIndex = 0; // Index of the current question
 
-const questionContainer = document.getElementById("question-container");
-const questionElement = document.getElementById("question");
-const userResponseElement = document.getElementById("user-response");
-const submitButton = document.getElementById("submit-button");
+    const questionContainer = document.getElementById("question-container");
+    const questionElement = document.getElementById("question");
+    const userResponseElement = document.getElementById("user-response");
+    const submitButton = document.getElementById("submit-button");
 
-const userResponses = []; // Initialize an array to store user responses
-
-// Function to display the current question
-function displayQuestion() {
-    if (currentQuestionIndex < questions.length) {
-        questionElement.textContent = questions[currentQuestionIndex];
-        userResponseElement.value = ""; // Clear the input field
-    } else {
-        questionContainer.style.display = "none"; // Hide the question container when all questions are answered
-        displayResults(); // Call a function to display results or provide feedback
+    // Function to display the current question
+    function displayQuestion() {
+        if (currentQuestionIndex < questions.length) {
+            questionElement.textContent = questions[currentQuestionIndex];
+            userResponseElement.value = ""; // Clear the input field
+        } else {
+            questionContainer.style.display = "none"; // Hide the question container when all questions are answered
+            displayResults(); // Call a function to display results or provide feedback
+        }
     }
-}
 
-// Event listener for the submit button
-submitButton.addEventListener("click", function () {
-    const userResponse = userResponseElement.value;
-    
-    // Process userResponse, e.g., save it or perform calculations
-    
-    userResponses.push(userResponse); // Store the user response in the array
+    // Event listener for the submit button
+    submitButton.addEventListener("click", function () {
+        const userResponse = userResponseElement.value;
+        
+        // Process userResponse, e.g., save it or perform calculations
+        
+        currentQuestionIndex++; // Move to the next question
+        displayQuestion(); // Display the next question
+    });
 
-    currentQuestionIndex++; // Move to the next question
-    displayQuestion(); // Display the next question
-});
-
-// Initialize the first question
-displayQuestion();
+    // Initialize the first question
+    displayQuestion();
 
 /*
 
